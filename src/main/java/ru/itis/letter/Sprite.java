c45559206ce573911121732eefe169bbf83f520e
am502 <antegzeperob@gmail.com>
2020-02-15 01:43:00 +0300

    add color sprite

--- src/main/java/ru/itis/letter/Sprite.java
+++ src/main/java/ru/itis/letter/Sprite.java
@@ -1,5 +1,7 @@
 package ru.itis.letter;
 
+import java.util.Arrays;
+
 public class Sprite {
     private int[] pixels;
 
@@ -10,5 +12,13 @@ public class Sprite {
     public static Sprite getLetterSprite(int row, int column) {
         int[] pixels = new int[Parameters.getPixelSize() * Parameters.getPixelSize()
                 * LetterSheet.LETTER_SIZE * LetterSheet.LETTER_SIZE];
+
+        return new Sprite(pixels);
+    }
+
+    public static Sprite getColorSprite(int height, int width, int color) {
+        int[] pixels = new int[height * width * Parameters.getPixelSize() * Parameters.getPixelSize()];
+        Arrays.fill(pixels, color);
+        return new Sprite(pixels);
     }
 }
