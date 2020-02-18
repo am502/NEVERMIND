f0e499181ae53e0a90545c5b0e65b0abde6cad78
am502 <antegzeperob@gmail.com>
2020-02-18 21:54:49 +0300

    rename from letter to symbol

--- src/main/java/ru/itis/letter/Sprite.java
+++ src/main/java/ru/itis/letter/Sprite.java
@@ -3,22 +3,22 @@ package ru.itis.letter;
 import java.util.Arrays;
 
 public class Sprite {
-    private int[] pixels;
+    private int[][] pixels;
 
-    public Sprite(int[] pixels) {
+    public Sprite(int[][] pixels) {
         this.pixels = pixels;
     }
 
     public static Sprite getLetterSprite(int row, int column) {
-        int[] pixels = new int[Parameters.getPixelSize() * Parameters.getPixelSize()
-                * LetterSheet.LETTER_SIZE * LetterSheet.LETTER_SIZE];
+        int[][] pixels = new int[Parameters.getPixelSize() * SymbolSheet.LETTER_SIZE]
+                [Parameters.getPixelSize() * SymbolSheet.LETTER_SIZE];
 
-        int rowIndex = 1 + LetterSheet.LETTER_SIZE * row + row;
-        int columnIndex = 1 + LetterSheet.LETTER_SIZE * column + column;
+        int rowIndex = 1 + SymbolSheet.LETTER_SIZE * row + row;
+        int columnIndex = 1 + SymbolSheet.LETTER_SIZE * column + column;
 
-        for (int i = 0; i < LetterSheet.LETTER_SIZE; i++) {
-            for (int j = 0; j < LetterSheet.LETTER_SIZE; j++) {
-//                int currentPixel = LetterSheet.getPixels()[] convert to matrix ?
+        for (int i = 0; i < SymbolSheet.LETTER_SIZE; i++) {
+            for (int j = 0; j < SymbolSheet.LETTER_SIZE; j++) {
+                int currentPixel = SymbolSheet.getPixels()[i + rowIndex][j + columnIndex];
             }
         }
 
@@ -26,8 +26,14 @@ public class Sprite {
     }
 
     public static Sprite getColorSprite(int height, int width, int color) {
-        int[] pixels = new int[height * width * Parameters.getPixelSize() * Parameters.getPixelSize()];
-        Arrays.fill(pixels, color);
+        int[][] pixels = new int[height * Parameters.getPixelSize()][width * Parameters.getPixelSize()];
+        for (int[] row : pixels) {
+            Arrays.fill(row, color);
+        }
         return new Sprite(pixels);
+    }
+
+    public int[][] getPixels() {
+        return pixels;
     }
 }
--- src/main/java/ru/itis/letter/SymbolSheet.java
+++ src/main/java/ru/itis/letter/SymbolSheet.java
@@ -5,24 +5,28 @@ import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class LetterSheet {
+public class SymbolSheet {
     public static final int LETTER_SIZE = 5;
 
-    private static int[] pixels;
+    private static int[][] pixels;
 
     static {
         try {
             BufferedImage image = ImageIO.read(new FileInputStream("/resources/letter/symbols.png"));
             int height = image.getHeight();
             int width = image.getWidth();
-            pixels = new int[height * width];
-            image.getRGB(0, 0, width, height, pixels, 0, width);
+            pixels = new int[height][width];
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    pixels[i][j] = image.getRGB(i, j);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static int[] getPixels() {
+    public static int[][] getPixels() {
         return pixels;
     }
 }
